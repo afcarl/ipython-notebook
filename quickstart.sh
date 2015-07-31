28caf6d2c752480b8e2928b5a0c5c5237af74fc4
Dmitri Nesterenko <dneseterenko@xogrp.com>
2015-07-30 22:22:55 -0500

    Add a mounted volume for host to container to map a notebooks folder

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 HOST_PORT=443
 CONTAINER_PORT=8888
-: ${PASSWORD:?"Need to set PASSWORD to a  non-empty value"
-}
-GOOD=`docker run -d -p $HOST_PORT:$CONTAINER_PORT -e "PASSWORD=$PASSWORD" ipython/notebook`
+HOST_DIR="/home/dmitri/volumes/notebooks"
+CONTAINER_DIR="/notebooks"
+: ${PASSWORD:?"Need to set PASSWORD to a  non-empty value"; exit 1;}
+
+docker run --name notebook -p $HOST_PORT:$CONTAINER_PORT -v $HOST_DIR:$CONTAINER_DIR -e "PASSWORD=$PASSWORD" ipython/notebook
 echo "Running notebook. On your host machine browse to https://127.0.0.1 and login with $PASSWORD"
